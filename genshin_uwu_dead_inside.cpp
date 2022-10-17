0cff3e3d97d38786034d920ef5531e9598830281
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2022-10-17 17:34:17 +0300

    Create genshin_uwu_dead_inside.cpp

---
+++ genshin_uwu_dead_inside.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1014; i > 5; i = i - 7) {
+        cout << i - 7 << " Andrei botskij botik" << "\n";
+        cout << "ded inside?" << "\n";
+        Sleep(10);
+    }
+}
